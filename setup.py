7adb428ae75673ebc349e8b231ee2236f7f11d88
jpranay7828 <jpranay7828@gmail.com>
2025-08-04 19:41:11 +0530

    setup detrails added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Information Retrieval System using Gen AI',
+    version = '0.0.0',
+    author = 'Pranay Joshi',
+    author_email =  'jpranay7828@gmail.com',
+    packages = find_packages(),
+    install_requires =  []
+)
